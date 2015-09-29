33f5b3f5af0ce3e039ff3d239dd28df620607a31
Hackbright Student <info@hackbrightacademy.com>
2015-09-28 17:20:57 -0700

    stripped out commas and question marks

--- wordcount.py
+++ wordcount.py
@@ -4,10 +4,11 @@ word_count = {}
 text = open("test.txt")
 
 for line in text:
-    line = line.strip()
-    line = line.split()
+    line = line.strip().replace(",", "").replace("?", "").split()
     for word in line:
         word_count[word] = word_count.get(word, 0) + 1
 
 for word, count in word_count.items():
     print "{} {}".format (word, count)
+
+text.close()
